e0570c9e2a034afc5ef81e4b2db3f6672b014326
Attila Horvath <sweeptic@gmail.com>
2023-05-30 11:46:42 +0200

    setting up DI between modules

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+    constructor(private powerService: PowerService) {
+
+    }
+}
